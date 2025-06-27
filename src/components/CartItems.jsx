f5a521073c767d90ad74f2a15c4dbc965a2c3707
ATAGWE ROGER <atagweroger2@gmail.com>
2025-06-27 11:11:09 +0100

    Worked mor on the cartcontainer adding the game image to he cart item too

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -7,6 +7,7 @@ function CartItems() {
 
   return (
     <div className={styles.cartItemsDiv}>
+        <div className={styles.cartDes}>{cart.length} GAME IN CART</div>
       <button
         onClick={() => setCartVisibility((prevState) => !prevState)}
         className={styles.closeBtn}
@@ -17,8 +18,18 @@ function CartItems() {
         {cart.map((item) => (
           <div id={item.id} className={styles.cartItem}>
             <div className={styles.cartitemcontainer}>
+                 <svg
+                              className={styles.gameImg}
+                              width="100%"
+                              height="100%"
+                              viewBox="0 0 320 160"
+                              preserveAspectRatio="xMidYMid slice"
+                            >
+                              <image href={item.background_image} width="100%" height="100%" />
+                            </svg>
                 <h2>{item.name}</h2>
-            <button onClick={() => removeFromCart(item) } className={styles.removeItemBtn}>X</button></div>
+            <button onClick={() => removeFromCart(item) } className={styles.removeItemBtn}>X</button>
+            </div>
              <hr />
           </div>
         ))}
